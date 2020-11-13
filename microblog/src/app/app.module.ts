8e15d60470fc94ca2ed6c0a39fc8af521e2c1626
abidtkg <abidtkg@gmail.com>
2020-11-14 02:37:24 +0600

    project reset

--- microblog/src/app/app.module.ts
+++ microblog/src/app/app.module.ts
@@ -1,35 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { CreateComponent } from './create/create.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    CreateComponent,
-    ProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-      { path: "", redirectTo: "home", pathMatch: "full" },
-      {path: "home", component: HomeComponent},
-      {path: "profile", component: ProfileComponent},
-      {path: "login", component: LoginComponent},
-      {path: "create", component: CreateComponent},
-
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
